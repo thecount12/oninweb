d0d11a20b939ff7662a5d9178272176c2cba8294
William Gunnells <wgunnells@box.com>
2019-08-01 18:09:43 -0700

    static files changes

--- oninweb/urls.py
+++ oninweb/urls.py
@@ -3,6 +3,9 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import include
 import basicapp.views 
 import signup.views
@@ -18,4 +21,4 @@ urlpatterns = [
 	path('thanks/', signup.views.thanks),
 	path('blog/', include('blog.urls')),
 	path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
