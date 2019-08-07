b505f6529309e437762fec62bc47089ee3f5bb6a
William Gunnells <gunnells@gmail.com>
2019-08-06 22:54:34 -0700

    missing details

--- supply/urls.py
+++ supply/urls.py
@@ -1,9 +1,13 @@
+"""
+supply urls
+"""
 
-from django.conf.urls import url
-
-from supply.views import CartridgeView
 from django.urls import path
+from supply.views import CartridgeView
+from supply.views import CartDetailView
+
 
 urlpatterns = [
-    path('cartridgeview/', CartridgeView.as_view(), name='cartridge-list'),
+    path('', CartridgeView.as_view(), name='cartridge-list'),
+    path('<int:pk>', CartDetailView.as_view(), name='cart-detail'),
 ]
--- supply/views.py
+++ supply/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+"""
+views
+"""
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from supply.models import Cartridge
 
 # Create your views here.
 
 
 class CartridgeView(ListView):
+    model = Cartridge
+
+
+class CartDetailView(DetailView):
     model = Cartridge
